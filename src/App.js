f934c5b6b6cdafcb66daa208f617455fd3f625c5
oktdib96 <oktdib96@gmail.com>
2022-04-11 00:20:37 +0200

    add methods related to adding elements

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import { StyledButton } from "./components/StyledButton";
 
 class App extends Component {
   state = {
+    text: "",
+    date: "",
     active: false,
     tasks: [
       {
         id: 1,
         text: "Zadanie1fdsdf  fdfsdfd fdsadasd sdadsadas",
-        date: "01.01.2023",
+        date: "2024-02-04",
       },
-      { id: 2, text: "Zadanie2", date: "01.01.2023" },
-      { id: 3, text: "Zadanie3", date: "01.01.2023" },
+      { id: 2, text: "Zadanie2", date: "2023-01-01" },
+      { id: 3, text: "Zadanie3", date: "2022-05-10" },
     ],
   };
   handleRemoveTask = (id) => {
@@ -30,6 +32,26 @@ class App extends Component {
     this.setState({
       active: !this.state.active,
     });
+  };
+  handleNewTask = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState((prevState) => ({
+      text: "",
+      data: "",
+      tasks: [
+        ...prevState.tasks,
+        {
+          id: this.state.tasks.length + 1,
+          text: this.state.text,
+          date: this.state.date,
+        },
+      ],
+    }));
   };
   render() {
     return (
@@ -46,7 +68,12 @@ class App extends Component {
           <StyledButton primary>Usuń wszystko</StyledButton>
         </div>
         {this.state.active ? (
-          <AddTaskPopup popup={this.handleShowPopup} />
+          <AddTaskPopup
+            data={this.state.tasks}
+            popup={this.handleShowPopup}
+            newTask={this.handleNewTask}
+            submit={this.handleSubmit}
+          />
         ) : null}
       </>
     );
--- src/components/AddTaskPopup.js
+++ src/components/AddTaskPopup.js
@@ -5,12 +5,23 @@ const AddTaskPopup = (props) => {
       <div className="close" onClick={() => props.popup()}>
         x
       </div>
-      <form>
+      <form onSubmit={props.submit}>
         <label>Treść zadania:</label>
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="wpisz zadanie..."
+          required={true}
+          name="text"
+          onChange={props.newTask}
+        />
         <label>Do kiedy należy zrobić</label>
-        <input type="date" />
-        <button>Dodaj zadanie!</button>
+        <input
+          type="date"
+          required={true}
+          name="date"
+          onChange={props.newTask}
+        />
+        <button type="submit">Dodaj zadanie!</button>
       </form>
     </div>
   );
